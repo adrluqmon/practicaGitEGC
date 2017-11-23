c14c0484677d1477b2064ecfe20c2726a65a0931
Adrian <adrluqmon@alum.us.es>
2017-11-23 16:33:56 +0100

    nuevo commit Please enter the commit message for your changes. Lines starting

--- java-hello/src/main/java/hello/GreetingController.java
+++ java-hello/src/main/java/hello/GreetingController.java
@@ -13,5 +13,11 @@ public class GreetingController {
         model.addAttribute("name", name);
         return "greeting";
     }
+	
+	@RequestMapping("/bye")
+    public String bye(@RequestParam(value="name", required=false, defaultValue="Universe") String name, Model model) {
+        model.addAttribute("name", name);
+        return "bye";
+    }
 
 }
